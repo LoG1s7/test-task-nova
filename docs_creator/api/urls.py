420dca7fdb599781b2bb87b6a295429c32432952
Alexander Kolesnikov <alexkolesnikov.git@gmail.com>
2024-03-03 20:14:45 +0300

    urls fix

--- docs_creator/api/urls.py
+++ docs_creator/api/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from rest_framework.authtoken import views
 
 from .views import upload_file
 
 urlpatterns = [
     path('upload_file/', upload_file),
-    path('api-token-auth/', views.obtain_auth_token),
 ]
